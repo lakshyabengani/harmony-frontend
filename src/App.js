7bcc1a24b0fce411631898026261aa2a4738e14c
Shuvam Shah <shvmks@outlook.com>
2021-03-30 02:12:17 +0530

    Appbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import "./styles/appbar.style.css";
+import Appbar from "./components/Appbar";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Appbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Appbar.js
@@ -0,0 +1,26 @@
+import { Nav, Navbar, Button } from "react-bootstrap";
+
+const Appbar = () => (
+  <Navbar collapseOnSelect expand="lg">
+    <Navbar.Brand href="#home">
+      <span className="appbar-title text-white">Harmony</span>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto"></Nav>
+      <Nav className="justify-content-end">
+        <Nav.Link href="#privacy" className="appbar-subtitle text-white">
+          Privacy
+        </Nav.Link>
+        <Nav.Link href="#about" className="appbar-subtitle text-white">
+          About
+        </Nav.Link>
+        <Button variant="secondary">
+          <span className="appbar-subtitle">LOG IN</span>
+        </Button>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default Appbar;
