6ab4fc89897167f4d437592c5ddd81ba940da485
Lakshya Bengani <lakshyabengani14@gmail.com>
2021-05-04 19:22:49 +0530

    basic skeleton of homepage done

--- src/components/CarouselCards.js
+++ src/components/CarouselCards.js
@@ -1,39 +1,38 @@
-import {Card,} from 'react-bootstrap';
-import {Carousel , Row} from 'react-bootstrap';
+import {Card,Carousel, Image } from 'react-bootstrap';
 import { peoplesList } from "../config";
 import '../styles/cards.style.css'
 
-function CarouselCards() {
+function CarouselCards(props) {
 
-     
      const renderCardList = () => 
      {
           const list = peoplesList.map( (person,idx) => (
                
-               <Card style={{ width: '100%' }} key={idx}>
-                      
+               <Card className='card' key={idx}>    
                     <Card.Body>
-                    <Carousel>
-                         <Carousel.Item>
-                              <Row>
-                              <img src={person.url} alt=""  height='420px'/>
-                              <img src='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSVvdf3mtAr8BQaBqwu2wAFbJD1dH6jtmyAK7hZRRnbFc0yc_pT' alt=""  height='420px'/>
-                              </Row>
-                         </Carousel.Item>
-                         <Carousel.Item>
-                              <img src={person.url} alt=""  height='420px'/>
-                         </Carousel.Item>
-                    </Carousel>
-                    <div style={{ overflowY:'scroll' , height :'150px'}}>
-                         <Card.Title>{person.name}, {person.age}</Card.Title>
-                              <Card.Text>
-                                   {person.info}
-                              </Card.Text>
-                    </div>
+                         <Carousel>
+                              {
+                                   person.url.map( imgUrl => 
+                                        <Carousel.Item>
+                                             <Image src={imgUrl} alt="" className='card-image' rounded/>
+                                        </Carousel.Item>)
+                              }
+                         </Carousel>
+                         <br/>
+                         <div className="container card-text" >
+                              <Card.Title>{person.name}, {person.age}</Card.Title>
+                                   <Card.Text>
+                                        {person.info}
+                                   </Card.Text>
+                         </div>
                     </Card.Body>
                </Card>
           )) 
-          return list[3];
+
+          if(props.index < list.length)
+               return list[props.index];
+          else 
+               props.setShow(false);
      }
 
      return (
